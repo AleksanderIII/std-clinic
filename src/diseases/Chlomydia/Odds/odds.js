9a4eb1e73ff277f05cefee6fac248478564f2e2b
Aleksandr Matyushik <34347587+AleksanderIII@users.noreply.github.com>
2021-06-20 03:36:41 +0300

    add mobile menu

--- src/diseases/Chlomydia/Odds/odds.js
+++ src/diseases/Chlomydia/Odds/odds.js
@@ -47,3 +47,31 @@ function toggleNavigator () {
   navigation.classList.toggle('closed')
 }
 
+
+const mobileMenuBtn = document.getElementById('mobile-menu-btn')
+const menuCloseBtn = document.getElementById('mobile-menu-btn-close')
+const mobileMenu = document.getElementById('mobile-menu')
+const headerContacts = document.getElementById('header-contacts')
+
+menuCloseBtn.addEventListener('click', toggleMenu)
+mobileMenuBtn.addEventListener('click', toggleMenu)
+function toggleMenu () {
+  mobileMenu.classList.toggle('hidden')
+  mobileMenuBtn.classList.toggle('hidden')
+  menuCloseBtn.classList.toggle('hidden')
+  headerContacts.classList.toggle('hidden')
+}
+
+const menuItems = mobileMenu.querySelectorAll('.menu-item')
+menuItems.forEach((menuItem, index) => {
+  if (menuItem.querySelectorAll('.menu-subfolder')) {
+    menuItem.addEventListener('click', function () {
+      menuItems.forEach((item, itemIndex) => {
+        if (index !== itemIndex) {
+          item.classList.add('hidden')
+        }
+      })
+      menuItem.classList.toggle('hidden')
+    })
+  }
+})
