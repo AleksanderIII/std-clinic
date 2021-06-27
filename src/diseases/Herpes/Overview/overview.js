bf27c85d7ed74e4f7a29a52febe9c785b241e135
Aleksandr Matyushik <34347587+AleksanderIII@users.noreply.github.com>
2021-06-27 19:35:27 +0300

    add fonts to herpes

--- src/diseases/Herpes/Overview/overview.js
+++ src/diseases/Herpes/Overview/overview.js
@@ -1,7 +1,6 @@
 import '../../../styles/Footer.css'
 import '../../../styles/Header.css'
 import '../../../styles/Diseases.css'
-import '../../../styles/index.css'
 import '../../../styles/haveAQuestion.css'
 import '../../../styles/overview.css'
 
